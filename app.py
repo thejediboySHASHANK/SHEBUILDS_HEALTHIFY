6ee621b440964d7074694385c27bbd56c1bf8204
thejediboySHASHANK <95047201+thejediboySHASHANK@users.noreply.github.com>
2023-01-06 21:29:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask, request, url_for, redirect, render_template
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-model=pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
@@ -12,17 +12,20 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features)]
     print(int_features)
     print(final)
-    prediction=model.predict(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    prediction = model.predict(final)
+    print(prediction)
+    output = '{0:.{1}f}'.format(prediction[0][0], 2)
+    output2 = '{0:.{1}f}'.format(prediction[0][1], 2)
+    # output2 = '{0:.{1}f}'.format(prediction2[0][1], 2)
 
-    
-    return render_template('index.html',pred='Required Calorie count is :  {}'.format(output),bhai="kuch karna hain iska ab?")
+    return render_template('index.html', pred='Required Step count is :  {}'.format(output), pred2='Required Calorie Count is : {}'.format(output2), bhai="kuch karna hain iska ab?")
+    # return render_template('index.html', pred2='Required Step count is :  {}'.format(output2), bhai="kuch karna hain iska ab?")
 
 
 
