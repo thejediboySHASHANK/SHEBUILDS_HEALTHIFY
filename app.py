3213f4c993070337242bd26528a4e6b4b5aa1c49
thejediboySHASHANK <95047201+thejediboySHASHANK@users.noreply.github.com>
2023-01-08 00:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,76 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask,request, url_for, redirect, render_template, jsonify
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+model=pickle.load(open('final_model-1.pkl','rb'))
+model2=pickle.load(open('model2.pkl','rb'))
 
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("forest.html")
+
+@app.route("/prediction", methods=["GET"])
+def prediction () : 
+    prediction = model.predict(request.json)
+
+    return prediction
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route ("/predictSteps", methods=["POST", "GET"])
+def predictsteps () :
+    features2 = [int(x) for x in request.form.values()]
+    final2 = [np.array(features2)]
+    print (features2)
+    print (final2)
+    prediction2 = model2.predict(final2)
+
+    output_steps = '{}'.format(prediction2[0][0], 2)
+
+    return render_template('steps.html',pred_steps='Steps required is  : {}'.format(output_steps), bhai="kuch karna hain iska ab?")
+    
+
+
+
+
+@app.route('/predict',methods=['POST','GET'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
-    final = [np.array(int_features)]
-    print(int_features)
+    # features = [1 if x == 'M' else 0 if x == 'F' else int(x) if x.isdigit() else  x for x in request.form.values()]
+
+    features = []
+    for x in request.form.values() :
+        if x == 'M' : 
+            value = 1
+        elif x == 'F' : 
+            value = 0       
+        else : 
+            try : 
+                value = int(x)
+            except ValueError : 
+                try : 
+                    value = float(x)
+                except ValueError : 
+                    value = x
+
+        features.append(value)
+    
+
+    final=[np.array(features)]
+    print(features)
     print(final)
-    prediction = model.predict(final)
-    print(prediction)
-    output = '{0:.{1}f}'.format(prediction[0][0], 2)
-    output2 = '{0:.{1}f}'.format(prediction[0][1], 2)
-    # output2 = '{0:.{1}f}'.format(prediction2[0][1], 2)
+    prediction=model.predict(final)
+    print (prediction)
 
-    return render_template('index.html', pred='Required Step count is :  {}'.format(output), pred2='Required Calorie Count is : {}'.format(output2), bhai="kuch karna hain iska ab?")
-    # return render_template('index.html', pred2='Required Step count is :  {}'.format(output2), bhai="kuch karna hain iska ab?")
+    # return jsonify({'prediction' : prediction})
+    
+    output1='{0:.{1}f}'.format(prediction[0][0], 2)
+    output2='{0:.{1}f}'.format(prediction[0][1], 2)
+    output3='{0:.{1}f}'.format(prediction[0][2], 2)
+    output4='{0:.{1}f}'.format(prediction[0][3], 2)
 
+    return render_template('index.html',pred1='BMI is : {}'.format(output1), pred2='BMR is {}' .format(output2), pred3='Calories is {}' .format(output3), pred4='Activity Level {}' .format(output4), bhai="kuch karna hain iska ab?")
 
 
 if __name__ == '__main__':
